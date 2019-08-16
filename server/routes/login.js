19ee1e75bb1c367efbbf13313bc60fdde3ecbd46
Manux <burban.dh@gmail.com>
2019-08-16 18:08:55 -0400

    Cambios en login js

--- server/routes/login.js
+++ server/routes/login.js
@@ -99,9 +99,69 @@ app.post('/google', async(req, res) => {
             });
         });
 
-
     Usuario.findOne({ email: googleUser.email }, (err, usuarioDB) => {
 
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (usuarioDB) {
+            if (usuarioDB.google === false) {
+                return res.status(400).json({
+                    ok: false,
+                    err: {
+                        message: 'Debe user su autenticacion normal'
+                    }
+                });
+
+            } else {
+
+                let token = jwt.sign({
+                    usuario: usuarioDB
+                }, process.env.SEED, { expiresIn: process.env.CADUCIDAD_TOKEN });
+
+                return res.json({
+                    ok: true,
+                    usuario: usuarioDB,
+                    token
+                })
+
+            }
+        } else {
+            //Si el usuario no existe en nuestra BBDD
+            let usuario = new Usuario();
+            usuario.nombre = googleUser.nombre;
+            usuario.email = googleUser.email;
+            usuario.img = googleUser.img;
+            usuario.google = true;
+            usuario.password = ':)';
+
+            usuario.save((err, usuarioDB) => {
+
+                if (err) {
+
+                    return res.status(500).json({
+                        ok: false,
+                        err
+                    });
+
+                }
+
+                let token = jwt.sign({
+                    usuario: usuarioDB
+                }, process.env.SEED, { expiresIn: process.env.CADUCIDAD_TOKEN });
+
+                return res.json({
+                    ok: true,
+                    usuario: usuarioDB,
+                    token
+                });
+
+            });
+        }
 
 
     });
